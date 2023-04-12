7d8a034a7296b9efd282058b12727d2003b9a2dc
diegosno <dserranoleal@gmail.com>
2023-04-12 16:16:42 +0200

    Added Math.floor to division in calculateCorrectAnswer

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,7 +88,7 @@ function calculateCorrectAnswer() {
   } else if (operator === "x") {
     return [operand1 * operand2, "multiply"];
   } else if (operator === "/") {
-    return [operand1 / operand2, "division"];
+    return [Math.floor(operand1 / operand2), "division"];
   } else if (operator === "-") {
     return [operand1 - operand2, "subtract"];
   } else {
@@ -135,8 +135,8 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 function displayDivisionQuestion(operand1, operand2) {
   document.getElementById("operand1").textContent =
-    operand1 > operand2 ? operand2 : operand1;
-  document.getElementById("operand2").textContent =
     operand1 > operand2 ? operand1 : operand2;
+  document.getElementById("operand2").textContent =
+    operand1 > operand2 ? operand2 : operand1;
   document.getElementById("operator").textContent = "/";
 }
