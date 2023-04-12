3b4372f7bf52ceafd729502a6a2a0ce6efa3d63b
diegosno <dserranoleal@gmail.com>
2023-04-12 11:55:00 +0200

    update script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByTagName("button");
 
-document.addEventListener("DOMContentLoaded"),
-function (){
-let buttons = document.getElementsByTagName("button");
-for (let button of buttons){
-button.addEventListener("click", function(){if(this.getAttribute("data-type")==="submit"){alert("You clicked Submit!");} })
-else { let gameType = this.getAttribute("data-type"); alert(`You clicked $(gameType)`)}}
-}
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked Submit!");
+      } else {
+        let gameType = this.getAttribute("data-type");
+        alert(`You clicked ${gameType}`);
+      }
+    });
+  }
+});
+
 function runGame() {}
+
 function checkAnswer() {}
 
 function calculateCorrectAnswer() {}
 
-function incrementScore() { }
+function incrementScore() {}
 
 function incrementWrongAnswer() {}
 
