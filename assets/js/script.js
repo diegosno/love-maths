fd51de217ffaefcd8b8b1b76b5697bb54a2da703
diegosno <dserranoleal@gmail.com>
2023-04-12 15:53:28 +0200

    edited division

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,8 +42,8 @@ function runGame(gameType) {
     displayAdditionQuestion(num1, num2);
   } else if (gameType === "multiply") {
     displayMultiplyQuestion(num1, num2);
-  } else if (gameType === "divide") {
-    displayDivideQuestion(num1, num2);
+  } else if (gameType === "division") {
+    displayDivisionQuestion(num1, num2);
   } else if (gameType === "subtract") {
     displaySubtractQuestion(num1, num2);
   } else {
@@ -88,7 +88,7 @@ function calculateCorrectAnswer() {
   } else if (operator === "x") {
     return [operand1 * operand2, "multiply"];
   } else if (operator === "/") {
-    return [operand1 / operand2, "divide"];
+    return [operand1 / operand2, "division"];
   } else if (operator === "-") {
     return [operand1 - operand2, "subtract"];
   } else {
@@ -133,7 +133,7 @@ function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById("operator").textContent = "x";
 }
 
-function displayDivideQuestion(operand1, operand2) {
+function displayDivisionQuestion(operand1, operand2) {
   document.getElementById("operand1").textContent =
     operand1 > operand2 ? operand2 : operand1;
   document.getElementById("operand2").textContent =
